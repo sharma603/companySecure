fe0baf1295e4f9f32a1926cb04ddeb8d42b5b5c7
Bijay Sharma <52573814+sharma603@users.noreply.github.com>
2025-08-09 20:48:17 +0300

    Delete resources directory

--- resources/views/layouts/app.blade.php
+++
@@ -1,6 +0,0 @@
-<!-- Replace school-related menu items with company ones -->
-<li class="nav-item">
-    <a class="nav-link" href="{{ route('admin.companies.index') }}">
-        Companies
-    </a>
-</li>
